30809aae1709426253417bc213e3325c9f7d9c9a
George Roberts <georgelroberts@hotmail.com>
2017-04-03 21:46:56 +0100

    PEP8 Standards and for loop removal for speedup
    Edited all of the code to abide by PEP8 standards. The for loop over the
    comment list is replaced by a call to grab the comment bodies in the
    list then joining the list to make one comment to search over.

--- redditCountries.py
+++ redditCountries.py
@@ -1,75 +1,93 @@
-## Testing Reddit's API: Search for number of times a country is mentioned
-# G. Roberts 2-4-17 
+# Testing Reddit's API: Search for number of times a country is mentioned
+# G. Roberts 2-4-17
 # Please visit https://www.reddit.com/prefs/apps/ to create an app and find out
 # your client_id and client_secret. user.
-# Searches only for official capitalised country names (althought I added England and America myself)
-# Geograpy is unfortunately incompatible with python 3.X, although if this were working it may be more versatile
-# TODO: Put this feature into google maps to visually show how much each county appears
+# Searches only for official capitalised country names
+# (althought I added England and America myself)
+# Geograpy is unfortunately incompatible with python 3.X,
+# although if this were working it may be more versatile
+#
+# TODO: Put this feature into google maps to visually
+# show how much each county appears
 
 import praw
 import matplotlib.pyplot as plt
 from operator import itemgetter
 import numpy as np
-import seaborn 
-seaborn.set() 
+import seaborn as sns
+sns.set_style("dark")
+
 
 def main():
-	reddit = praw.Reddit(client_id='yourID',
-					client_secret='yourSecret',
-					user_agent='testscript by /u/yourUserName')
-	countryDict=importCountries()
+    reddit = praw.Reddit(
+        client_id='publicID',
+        client_secret='secretkey',
+        user_agent='testscript by /u/username')
+    # reddit = praw.Reddit(client_id='yourID',
+    #               client_secret='yourSecret',
+    #               user_agent='testscript by /u/yourUserName')
+    countryDict = importCountries()
 
-	subName='worldnews'
-	numberOfPosts=200
+    subName = 'news'
+    numberOfPosts = 150
+    posts = reddit.subreddit(subName).hot(limit=numberOfPosts)
 
-	posts = reddit.subreddit(subName).hot(limit=numberOfPosts)
+    for post in posts:
+        submission = reddit.submission(id=post.id)
+        submission.comments.replace_more(limit=0)
+        individualComment = submission.comments.list()
+        commentList = [o.body for o in individualComment]
+        commentList = ' '.join(commentList)
+        for country in countryDict:
+            countryMentions = commentList.count(country)
+            countryDict[country] = countryDict[country] + countryMentions
 
-	for post in posts:
-		submission=reddit.submission(id=post.id)
-		submission.comments.replace_more(limit=0)
-		individualComment=submission.comments.list()
-		for comment in individualComment:
-			for country in countryDict:
-				countryMentions=comment.body.count(country)
-				countryDict[country]=countryDict[country]+countryMentions
+    # Want to remove unmentioned countries for plotting
+    for key in list(countryDict):
+        if countryDict[key] == 0:
+            del countryDict[key]
 
-	# Want to remove unmentioned countries for plotting
-	for key in list(countryDict):
-		if countryDict[key]==0:
-			del countryDict[key]
-	
-	plotDict(countryDict,15, subName, numberOfPosts)
-	
+    plotDict(countryDict, 15, subName, numberOfPosts)
+
+    return countryDict, posts, submission, individualComment, commentList
+
+
 def importCountries():
-	"""Imports a text file countaining all countries and their code"""
-	rawCountries=open("countries.txt","r")
-	rawCountriesList=rawCountries.readlines()
-	countryDict={}
-	for country in rawCountriesList:
-		country=country.strip('\n')
-		country=country[3:]
-		countryDict[country]=0
-	return countryDict
+    """Imports a text file countaining all countries and their code"""
+    rawCountries = open("countries.txt", "r")
+    rawCountriesList = rawCountries.readlines()
+    countryDict = {}
+    for country in rawCountriesList:
+        country = country.strip('\n')
+        country = country[3:]
+        countryDict[country] = 0
+    return countryDict
 
-def plotDict(dictToPlot,numberOfCountries,subName, numberOfPosts):
-	"""Converts a dictionary to a list and then creates a bar graph"""
-	countryList=list(dictToPlot.items())
-	countryList=sorted(countryList,key=itemgetter(1), reverse=True)
-	if len(countryList)>numberOfCountries:
-		countryList=countryList[0:numberOfCountries]
-	x=list(zip(*countryList))[0]
-	y=list(zip(*countryList))[1]
-	x_pos=np.arange(len(x))
 
-	plt.bar(x_pos, y,align='center')
-	plt.xticks(x_pos, x, rotation=70, fontsize=14) 
-	plt.ylabel('Number of Mentions', fontsize=16)
-	plt.xlabel("Country ", fontsize=16)
-	plt.title('Country mentions in /r/'+subName+' comments from '+str(numberOfPosts)+' posts')
-	plt.margins(0.05, 0.01)
-	#plt.axis('tight')
-	plt.tight_layout()
-	plt.show()
+def plotDict(dictToPlot, numberOfCountries, subName, numberOfPosts):
+    """Converts a dictionary to a list and then creates a bar graph"""
+    countryList = list(dictToPlot.items())
+    countryList = sorted(countryList, key=itemgetter(1), reverse=True)
+    if len(countryList) > numberOfCountries:
+        countryList = countryList[0:numberOfCountries]
+    x = list(zip(*countryList))[0]
+    y = list(zip(*countryList))[1]
+    x_pos = np.arange(len(x))
 
-if __name__=="__main__":
-	main()
+    plt.bar(x_pos, y, align='center')
+    plt.xticks(x_pos, x, rotation=70, fontsize=14)
+    plt.ylabel('Number of Mentions', fontsize=16)
+    plt.xlabel("Country ", fontsize=16)
+    plt.title(
+        'Country mentions in /r/' + subName +
+        ' comments from ' + str(numberOfPosts) + ' posts',
+        fontsize=18)
+    plt.margins(0.05, 0.01)
+    # plt.style.use('bmh')
+    # plt.axis('tight')
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    countryDict, posts, submission, individualComment, commentList = main()
